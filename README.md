1c2e4b5c13e2c1776a80dd81444099de4838d1e6
ddubuk <jh.k51@gimjeonghyeon-ui-macbook-pro.local>
2018-03-19 11:20:53 +0900

    .

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # java106
 비트캠프 강의 소스
 # bitcamp
+# bitcamp
--- java-basic/src/step07/Exam02_2.java
+++ java-basic/src/step07/Exam02_2.java
@@ -1,18 +1,21 @@
-// 클래스 용도 - 소스 코드를 관리하기 쉽게 관련된 기능을 묶을 때 사
+// 메서드 분류 - 클래스 변수의 활용  
 package step07;
 
-public class Exam01_2 {
+public class Exam02_2 {
     public static void main(String[] args) {
-        int result = 0;
+        // 계산 결과를 보관할 변수는 더이상 필요가 없다.
+        //Calculator2 내부에서 계산 결과를 관리한다.
+        //int result = 0;
         
         // 2 + 3 - 1 * 7 / 3 = ?
         // => 연산자 우선 순위를 고려하지 않고 순서대로 계산하라!
-        result = Calculator.plus(2, 3);
-        result = Calculator.minus(result, 1);
-        result = Calculator.multiple(result, 7);
-        result = Calculator.divide(result, 3);
+        Calculator.plus(2);
+        Calculator.plus(3);
+        Calculator.minus(1);
+        Calculator.multiple(7);
+        Calculator.divide(3);
         
-        System.out.printf("result = %d\n", result);
+        System.out.printf("result = %d\n",Calculator2.result);
 
     }
 }
