42d15f831b5ee7ade3be000a40fe60d6cfb625e7
ddubuk <adam0513k@gmail.com>
2018-04-19 16:52:34 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/controller/board/BoardAddController.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/controller/board/BoardAddController.java
@@ -23,6 +23,10 @@ public class BoardAddController implements Controller {
         board.setTitle(request.getParameter("title"));
         board.setContent(request.getParameter("content"));
         board.setCreatedDate(new Date(System.currentTimeMillis()));
+        
+        for (int i = 0; i < 300000000; i++) {
+            double d = Math.asin(32.8);
+        }
         boardDao.insert(board);
         
         PrintWriter out = response.getWriter();
